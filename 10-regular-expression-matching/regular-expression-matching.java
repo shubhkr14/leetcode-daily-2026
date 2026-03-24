fb83042b76902f450b983f1838e811e494f7ab8d
Shubham Kumar <shubhk1403@gmail.com>
2026-03-24 17:13:10 +0530

    Time: 1119 ms (5.02%) | Memory: 47.2 MB (5.28%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,33 +1,16 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length(), n = p.length();
-        boolean[][] dp = new boolean[m + 1][n + 1];
-
-        dp[0][0] = true;
-
-        // Handle patterns like a*, a*b*, a*b*c*
-        for (int j = 2; j <= n; j++) {
-            if (p.charAt(j - 1) == '*') {
-                dp[0][j] = dp[0][j - 2];
-            }
+        return solve(s,p);
+    }
+    public boolean solve(String s, String p){
+        if(p.length()==0) return s.length()==0;
+        boolean first_char_matched=false;
+        if(s.length()>0 && (p.charAt(0)=='.' || p.charAt(0)==s.charAt(0))) first_char_matched=true;
+        if(p.length() >= 2 && p.charAt(1)=='*'){
+           boolean not_take=solve(s, p.substring(2));
+           boolean take=first_char_matched && solve(s.substring(1),p);
+           return not_take || take; 
         }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-
-                if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } 
-                else if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i][j - 2]; 
-
-                    if (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)) {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
-                    }
-                }
-            }
-        }
-
-        return dp[m][n];
+        return first_char_matched && solve(s.substring(1), p.substring(1));
     }
 }
