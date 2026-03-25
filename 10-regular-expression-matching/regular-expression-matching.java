d4da7835754dd6f60a4959474a9dcfb937bc2c52
Shubham Kumar <shubhk1403@gmail.com>
2026-03-25 11:54:06 +0530

    Time: 1078 ms (5.02%) | Memory: 47.1 MB (5.30%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -2,15 +2,15 @@ class Solution {
     public boolean isMatch(String s, String p) {
         return solve(s,p);
     }
-    public boolean solve(String s, String p){
+    private boolean solve(String s, String p){
         if(p.length()==0) return s.length()==0;
-        boolean first_char_matched=false;
-        if(s.length()>0 && (p.charAt(0)=='.' || p.charAt(0)==s.charAt(0))) first_char_matched=true;
-        if(p.length() >= 2 && p.charAt(1)=='*'){
-           boolean not_take=solve(s, p.substring(2));
-           boolean take=first_char_matched && solve(s.substring(1),p);
-           return not_take || take; 
+        boolean firstCharMatch=false;
+        if(s.length()>0 && (p.charAt(0)==s.charAt(0) || p.charAt(0)=='.')) firstCharMatch=true;
+        if(p.length()>=2 && p.charAt(1)=='*'){
+            boolean not_take=solve(s,p.substring(2));
+            boolean take=firstCharMatch && solve(s.substring(1),p);
+            return not_take || take;
         }
-        return first_char_matched && solve(s.substring(1), p.substring(1));
+        return firstCharMatch && solve(s.substring(1),p.substring(1));
     }
 }
