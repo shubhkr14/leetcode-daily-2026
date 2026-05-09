65411c61ea89328a09b92987f9ef34e55e864af1
Shubham Kumar <shubhk1403@gmail.com>
2026-05-09 13:23:24 +0530

    Time: 4 ms (60.82%) | Memory: 45.4 MB (39.98%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int bit = 0; bit < 32; bit++) {
+            int count = 0;
+
+            for (int num : nums) {
+                if (((num >> bit) & 1) == 1) {
+                    count++;
+                }
+            }
+
+            if (count % 3 != 0) {
+                result |= (1 << bit);
+            }
+        }
+        return result;
+    }
+}
