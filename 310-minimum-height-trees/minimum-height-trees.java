c5d7e8b86c17f9f903e7c25cd32907a58852736f
Shubham Kumar <shubhk1403@gmail.com>
2026-06-03 14:17:51 +0530

    Time: 31 ms (32.52%) | Memory: 74.1 MB (22.11%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -3,43 +3,31 @@ class Solution {
         if (n == 1) {
             return Arrays.asList(0);
         }
-
         List<Set<Integer>> graph = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             graph.add(new HashSet<>());
         }
-
         for (int[] edge : edges) {
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
-
         List<Integer> leaves = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             if (graph.get(i).size() == 1) {
                 leaves.add(i);
             }
         }
-
         int remainingNodes = n;
-
         while (remainingNodes > 2) {
             remainingNodes -= leaves.size();
-
             List<Integer> newLeaves = new ArrayList<>();
-
             for (int leaf : leaves) {
                 int neighbor = graph.get(leaf).iterator().next();
-
                 graph.get(neighbor).remove(leaf);
-
                 if (graph.get(neighbor).size() == 1) {
                     newLeaves.add(neighbor);
                 }
             }
-
             leaves = newLeaves;
         }
 
