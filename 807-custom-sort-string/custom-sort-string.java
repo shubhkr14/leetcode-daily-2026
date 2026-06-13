809d535643d8a061992863bfec4d1de6e16f14d5
Shubham Kumar <shubhk1403@gmail.com>
2026-06-13 08:42:47 +0530

    Time: 1 ms (91.66%) | Memory: 43.1 MB (37.21%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : order.toCharArray()) {
+            while (freq[c - 'a'] > 0) {
+                sb.append(c);
+                freq[c - 'a']--;
+            }
+        }
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            while (freq[c - 'a'] > 0) {
+                sb.append(c);
+                freq[c - 'a']--;
+            }
+        }
+
+        return sb.toString();
+    }
+}
